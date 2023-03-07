ba102d8aea321725a442868e8ccef663966fd55d
3rick <erick.dejesus1@alumno.buap.mx>
2023-03-06 21:27:29 -0600

    Grafo para Cabbage Wolf Goat and Farmer

--- app/mapper.py
+++ app/mapper.py
@@ -12,17 +12,30 @@ for line in lines:
     # print(f"key: {head} => value: {tail}")
     dic[head] = tail
 
-# export this function and use it in another module
-def get_neighbors(node):
-    return dic[node]
-
-if __name__ == "__main__":
-    G = nx.Graph()
+def create_graph():
+    G = nx.DiGraph()
     for key, value in dic.items():
         G.add_node(key)
         for v in value:
             G.add_edge(key, v)
+    return G
+# export this function and use it in another module
+def get_neighbors(node):
+    return dic[node]
 
-    pos = nx.spring_layout(G)
-    nx.draw(G, pos, with_labels=True)
+# plot the graph implementing Depth First Search
+def dfs(G, start):
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(set(G[vertex]) - visited)
+    return visited
+
+if __name__ == "__main__":
+    G = create_graph()
+    # pick a tree-like layout
+    # pos = nx.
+    nx.draw_planar(G, node_size=1000, with_labels=True, font_size=14)
     plt.show()
